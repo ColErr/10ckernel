8ba6d3daa6a31ff51cb9e29dab5eb25b56c623bd
ColErr <jkripper@gmail.com>
2012-10-28 21:36:05 -0700

    Editted some things from SirCmpwn's reccomendations

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,6 @@
 @echo off    
 mkdir ..\bin
 mkdir ..\debug
-organic.exe ../src/main.dasm ../bin/kernel.bin -w ../src -l ../debug/kernel.lst
+organic.exe ../src/main.dasm ../bin/kernel.bin -w ../src -l ../debug/kernel.lst -i ../inc
 pause
 @echo on
