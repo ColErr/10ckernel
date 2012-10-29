94fe9ec0b7ab2057cb4413f319726b8d49dd99af
ColErr <jkripper@gmail.com>
2012-10-28 20:18:23 -0700

    Readme, couple issues in memory, and fixed up some build stuff

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,6 @@
 @echo off    
 mkdir ..\bin
 mkdir ..\debug
-organic.exe ../src/main.dasm ../bin/kernel.bin -w ../src -l ../debug/kernel.lst -i ../src/drivers
+organic.exe ../src/main.dasm ../bin/kernel.bin -w ../src -l ../debug/kernel.lst
 pause
 @echo on
